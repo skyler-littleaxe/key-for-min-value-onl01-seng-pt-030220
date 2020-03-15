dbd3ae28af819d8bdcf80d3eaeb30dcacde751d3
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2020-03-15 04:01:47 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,9 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  smallest_box_label = nil
-  smallest_box_size = nil
+  smallest_name = nil
+  smallest_num = nil
   name_hash.each do |k, v|
+    if smallest_box_size < v
   
 end
 end
